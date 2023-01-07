ce66e97c4a9055e649562235b38587590e772b85
AbdullahAlmizan644 <abdullahalmizan644@gmail.com>
2023-01-07 21:25:43 +0600

    final

--- home/admin.py
+++ home/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import Contact,Category,Blog,Team,Tournament,Match,Pointable,Comment,Match_Video
 
+
+admin.site.index_title="Game Maker Dashboard"
+admin.site.site_title="Game Maker Dashboard"
+admin.site.site_header="Admin-Panel"
 # Register your models here.
 admin.site.register(Contact)
 admin.site.register(Category)
